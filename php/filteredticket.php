85421082f56797754ab1abaaad8b5ce1ac677eb7
Görkem Deniz SEYHAN <69456932+Denowen@users.noreply.github.com>
2020-12-27 17:25:29 +0300

    Update filteredticket.php

--- php/filteredticket.php
+++ php/filteredticket.php
@@ -151,23 +151,29 @@
 </head>
 <body>    
     <?php
+    session_start();
     include 'dbconnect.php';
     $ticket_ = $_POST['ticket'];
     $way_ = $_POST['way'];
+    $_SESSION['way'] = $way_;
     $nereden_ = $_POST['nereden'];
     $nereye_ = $_POST['nereye'];
     $sdate_ = $_POST['sdate'];
     $edate_ = $_POST['edate'];
-    $query = "SELECT * FROM Route WHERE dateOfRoute = '$sdate_' and startingStationTerminal = '$nereden_' and destinationStationTerminal = '$nereye_'";
+
     if ($way_ == "çift") {
         $query2 = "SELECT * FROM Route WHERE dateOfRoute = '$edate_' and startingStationTerminal = '$nereye_' and destinationStationTerminal = '$nereden_'";
         $result2 = mysqli_query($conn, $query2);
         $num2 = mysqli_num_rows($result2);
     }
-
-    $result = mysqli_query($conn, $query);
-    $num = mysqli_num_rows($result);
+    if ($way_ == 'tek' || $way_ == 'çift') {
+        $query = "SELECT * FROM Route WHERE dateOfRoute = '$sdate_' and startingStationTerminal = '$nereden_' and destinationStationTerminal = '$nereye_'";
+        $result = mysqli_query($conn, $query);
+        $num = mysqli_num_rows($result);
+    }
     mysqli_close($conn);
+    
+    
     ?>
 <div class="page">
     <div class="navbar">
@@ -188,85 +194,101 @@
         <div class="contain">
             <div class="c-row">
                 <h3>Filtreleme Sonucu: </h3>
+                <form method="get" action="selectseats.php">
+     <input type="hidden" name="way" value='$way_'>
+    
+        </form>
             </div>
             <form class ="d-row" action="fillinfo.php" method='post'>
                 <table cellpadding="10">
-                    <h4>Gidiş</h4>
-                    <tr>
-                        <th>Tarih</th>
-                        <th>Nereden</th>
-                        <th>Nereye</th>
-                        <th>Saat</th>
-                        <th></th>
-                    </tr>
 
-                    <?php
-                    $i = 0;
-                    while ($i < $num) {
-                        while ($count = mysqli_fetch_array($result)) {
-                            ?>
 
-                            <tr>
+<?php
+$i = 0;
+if ($way_ == "çift" || $way_ == "tek") {
 
-                                <td><?php echo $count['dateOfRoute'] ?></td>
-                                <td><?php echo $count['startingStationTerminal'] ?></td>
-                                <td><?php echo $count['destinationStationTerminal'] ?></td>
-                                <td><?php echo $count['departureTime'] ?></td>
-                                <td><input type="radio" name="route1" value= '<?php echo $count['routeId'] ?>' /></td>
+    echo" <h4>Gidiş</h4>";
+    echo"<tr>";
+    echo "<th>Tarih</th>";
+    echo "<th>Nereden</th>";
+    echo "<th>Nereye</th>";
+    echo "<th>Saat</th>";
+    echo "<th>Fiyat</th>";
+    echo "<th></th>";
+    echo "</tr>";
 
-                            </tr>
-                            <?php
-                            $i++;
-                        }
-                    }
-                    ?>
+    while ($i < $num) {
+        while ($count = mysqli_fetch_array($result)) {
+            ?>                  <tr>
+
+                                    <td><?php echo $count['dateOfRoute'] ?></td>
+                                    <td><?php echo $count['startingStationTerminal'] ?></td>
+                                    <td><?php echo $count['destinationStationTerminal'] ?></td>
+                                    <td><?php echo $count['arrivelTime'] ?></td>
+                                    <td><?php echo $count['price'] ?></td>
+                                    <td><input type="radio" name="route1" value= '<?php echo $count['routeId'] ?>' />
+                                    <input type="hidden" name="trainNumber" value='<?php echo $count['trainNumber'] ?>'>
+                                </td>
+
+                                    
+
+                                </tr>
+            <?php
+            $i++;
+        }
+    }
+}
+?>
                 </table>
 
                 <table cellpadding="10">
-                    
+
                     <?php
                     $i = 0;
                     if ($way_ == "çift") {
-                        
-                       echo" <h4>Dönüş</h4>";
-                    echo"<tr>";
-                     echo "<th>Tarih</th>";
-                       echo "<th>Nereden</th>";
-                       echo "<th>Nereye</th>";
-                        echo "<th>Saat</th>";
-                       echo "<th></th>";
-                   echo "</tr>";
-                    
-                    while ($i < $num2) {
-                        while ($count = mysqli_fetch_array($result2)) {
-                            ?>
-                    
-                            <tr>
-                                <td><?php echo $count['dateOfRoute'] ?></td>
-                                <td><?php echo $count['startingStationTerminal'] ?></td>
-                                <td><?php echo $count['destinationStationTerminal'] ?></td>
-                                <td><?php echo $count['departureTime'] ?></td>
-                                <td><input type="radio" name="route2" value='<?php echo $count['routeId'] ?>' /></td>
-                            </tr>
 
-                            <?php
-                            $i++;
-                        }
-                    }
-                    }
-                    
-                    ?>
+                        echo" <h4>Dönüş</h4>";
+                        echo"<tr>";
+                        echo "<th>Tarih</th>";
+                        echo "<th>Nereden</th>";
+                        echo "<th>Nereye</th>";
+                        echo "<th>Saat</th>";
+                        echo "<th>Fiyat</th>";
+                        echo "<th></th>";
+                        echo "</tr>";
+
+                        while ($i < $num2) {
+                            while ($count = mysqli_fetch_array($result2)) {
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $count['dateOfRoute'] ?></td>
+                                    <td><?php echo $count['startingStationTerminal'] ?></td>
+                                    <td><?php echo $count['destinationStationTerminal'] ?></td>
+                                    <td><?php echo $count['arrivelTime'] ?></td>
+                                    <td><?php echo $count['price'] ?></td>
+                                    <td><input type="radio" name="route2" value= '<?php echo $count['routeId'] ?>' />
+                                    <input type="hidden" name="trainNumber2" value='<?php echo $count['trainNumber'] ?>'>
+                                </td>
+                                </tr>
+
+            <?php
+            $i++;
+        }
+    }
+}
+?>
                 </table>
 
 
                 <div class ="a-row">
                     <div class = "Devam Et">
                         <input type="submit" name ='devam et' value="Devam Et" style="background-color: #E5FFCC;
-                                   border-radius: 1rem;
-                                   border-color: green;
-                                   width: 6rem;
-                                   height: 2rem;"/>
-                        </div>
+                               border-radius: 1rem;
+                               border-color: green;
+                               width: 6rem;
+                               height: 2rem;"/>
+                    </div>
 
                 </div>
             </form>
