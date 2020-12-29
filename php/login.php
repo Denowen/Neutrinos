4c5f6a18889319fd19fa8c05cc80be96f7225b3e
Görkem Deniz SEYHAN <69456932+Denowen@users.noreply.github.com>
2020-12-29 22:01:43 +0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -16,10 +16,10 @@ if(isset($_POST['login'])){
 
     if(mysqli_num_rows($result1) ==1 and mysqli_num_rows($result2) == 0){
         $_SESSION['email']=$email;
-        header('location: home2.php');
+        header('location: home.php');
     } else if (mysqli_num_rows($result2) == 1){
         $_SESSION['email']=$email;
-        header('location: home2.php');
+        header('location: home.php');
     }else {
         
         echo("<br>wrong id pass");
