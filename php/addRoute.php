ec332c28d9a774e86281f9dfc934f02b8726ad54
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-05 21:06:30 +0300

    Update addRoute.php

--- php/addRoute.php
+++ php/addRoute.php
@@ -27,16 +27,3 @@ if(empty($nereden)|| empty($nereye)|| empty($saat)|| empty($trainNumber) || empt
  }
 
 
-//if(empty($drop)){
-//
-//    $query2 = "DELETE FROM ticket WHERE PNR='$cancel'";
-//    $result2 = mysqli_query($conn, $query2);
-//    $query3 = "DELETE FROM buy WHERE PNR='$cancel'";
-//    $result3 = mysqli_query($conn, $query3);
-//    header("Location:user.php");
-//
-//}else{
-//    $query4 = "DELETE FROM reserve WHERE reservationId='$drop'";
-//    $result4 = mysqli_query($conn, $query4);
-//    header("Location:user2.php");
-//}
