a1a50b36f1e979606f517c04384103b362d391ed
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-06 16:42:31 +0300

    Update addRoute.php

--- php/addRoute.php
+++ php/addRoute.php
@@ -7,23 +7,21 @@ $nereye = $_POST['nereye'];
 $saat = $_POST['saat'];
 $trainNumber = $_POST['trainNumber'];
 $date = $_POST['date'];
+$price = $_POST['price'];
 
-$routeId= "SELECT max(routeId) from route";
+$routeId = "SELECT max(routeId) from route";
 $route = mysqli_query($conn, $routeId);
 $row = mysqli_fetch_array($route);
-$row[0] +=1;
-$saat2 = $saat+2;
+$row[0] += 1;
+$saat2 = $saat + 2;
 
 
 
-if(empty($nereden)|| empty($nereye)|| empty($saat)|| empty($trainNumber) || empty($trainNumber)){
-     header("location: admin.php");
- }
- else{
+if (empty($nereden) || empty($nereye) || empty($saat) || empty($trainNumber) || empty($trainNumber) || empty($price)) {
+   header("location: admin.php");
+} else {
 
-    $query ="INSERT INTO route VALUES('1','$nereden', '$nereye', '$saat:00', '$saat2:00', '$date', '$trainNumber', '50')";
-    $result= mysqli_query($conn, $query);
-    echo $row[0];
- }
-
-
+   $query = "INSERT INTO route VALUES('$row[0]','$nereden', '$nereye', '$saat:00', '$saat2:00', '$date', '$trainNumber', '$price')";
+   $result = mysqli_query($conn, $query);
+   echo $price;
+}
