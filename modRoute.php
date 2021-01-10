e1631d958314135d9b7162c461ddef7ca1cf2b71
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 14:50:09 +0300

    Update modRoute.php

--- modRoute.php
+++ modRoute.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
-include 'dbconnect.php';
+include('dbconnect.php');
+
+if (isset($_SESSION['loggedin'])) {
+    $email = $_SESSION['email'];
+    //QUERY THAT CHECKS IF THE VIEWER IS AN ADMIN
+    $query = mysqli_query($conn, "select * from registeredusers where regUserEmail = '$email'");
+    $counta = mysqli_num_rows($query);
+    if (!$counta) {
+        header("location:admin.php");
+    } 
+}
+
+
+?>
+
+
+<?php
+
 
 $routeId = $_POST['routeId'];
 $saat = $_POST['saat'];
