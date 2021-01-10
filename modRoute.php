4b94e99116c04301258a1d80bfe8fc609bc3993f
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 20:28:09 +0300

    Update modRoute.php

--- modRoute.php
+++ modRoute.php
@@ -14,7 +14,27 @@ if (empty($saat) || empty($date) ||  empty($price)) {
    header("location: modifyRoute.php?varname=$routeId");
 } else {
 
+   $trains = "SELECT * FROM route WHERE trainNumber='$trainNumber' and dateOfRoute = '$date'";
+   $q1 = mysqli_query($conn, $trains);
+
+
+   $count = 0;
+
+   while ($array = mysqli_fetch_array($q1, MYSQLI_ASSOC)) {
+      if ($array['departureTime'] < $saat and $array['arrivelTime'] < $saat and $saat2<$saat and $array['startingStationTerminal'] == $$array['destinationStationTerminal']) {
+         $count++;
+      }
+   }
+   if ($count == 0) {
+
    $query = "UPDATE route SET departureTime ='$saat:00', arrivelTime ='$saat2:00', dateOfRoute ='$date', price='$price' where routeId='$routeId'";
    $result = mysqli_query($conn, $query);
    header("location: allRouteList.php");
+}
+else{
+   echo '<script type="text/javascript">'; 
+echo 'alert("Yanlış bilgi girdiniz.");'; 
+echo 'window.location.href = "admin.php";';
+echo '</script>';
+}
 }
