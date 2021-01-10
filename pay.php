2295b2145d4dfd474904ece7e0862a4812ad9cdb
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 14:51:45 +0300

    Update pay.php

--- pay.php
+++ pay.php
@@ -8,9 +8,26 @@
     <link rel="stylesheet" type="text/css" href="pay.css">
 
 </head>
+    
+    <?php
+session_start();
+include('dbconnect.php');
+
+if (isset($_SESSION['loggedin'])) {
+    $email = $_SESSION['email'];
+    //QUERY THAT CHECKS IF THE VIEWER IS AN ADMIN
+    $query = mysqli_query($conn, "select * from registeredusers where regUserEmail = '$email'");
+    $counta = mysqli_num_rows($query);
+    if (!$counta) {
+        header("location:admin.php");
+    } 
+}
+
+
+?>
+
 <body>    
-   <?php
-   session_start();
+
    include 'dbconnect.php';
    
 
