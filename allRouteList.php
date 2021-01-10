ce5d3aeb6d51a354d81e300060a3b3cbfd29d5f9
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 21:17:10 +0300

    Update allRouteList.php

--- allRouteList.php
+++ allRouteList.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <?php include 'dbconnect.php';
-    session_start();
-    $email = $_SESSION['email'];
-    //QUERY THAT CHECKS IF THE VIEWER IS AN ADMIN
-    $query = mysqli_query($conn, "select * from administrators where adminEmail = '$email'");
-    $counta = mysqli_num_rows($query);
-    if (!$counta) {
-        header("location:home.php");
-    } 
+session_start();
+$email = $_SESSION['email'];
+//QUERY THAT CHECKS IF THE VIEWER IS AN ADMIN
+$query = mysqli_query($conn, "select * from administrators where adminEmail = '$email'");
+$counta = mysqli_num_rows($query);
+if (!$counta) {
+    header("location:home.php");
+}
 
 
 ?>
@@ -45,29 +45,29 @@
 
                     </div>
                     <div class="p-ticket">
-                        <a class="a2" href="addNewRoute.php">Add new route</a>
+                        <a class="a2" href="addNewRoute.php">Yeni Rota Ekle</a>
                     </div>
                     <div class="p-ticket">
-                        <a class="a2" href="allRouteList.php">All route list</a>
+                        <a class="a2" href="allRouteList.php">Rotaları Listele</a>
                     </div>
                     <div class="p-ticket">
-                        <a class="a2" href="allPassengers.php">See All Passengers</a>
+                        <a class="a2" href="allPassengers.php">Yolcuları Listele</a>
                     </div>
                     <div class="p-ticket">
-                        <a class="a2" href="allTicketList.php">All Ticket List</a>
+                        <a class="a2" href="allTicketList.php">Biletleri Listele</a>
                     </div>
                 </div>
                 <div class="right2">
                     <table cellpadding="20" style="color: rgb(50 239 239 / 68%);flex: 2;">
                         <thead>
                             <th>Id</th>
-                            <th>Departure</th>
-                            <th>Destination</th>
-                            <th>Arrivel Time</th>
-                            <th>Departure Time</th>
-                            <th>Date of the Route</th>
-                            <th>Train Number</th>
-                            <th>Price</th>
+                            <th>Kalkış</th>
+                            <th>Varış</th>
+                            <th>Kalkış Saati</th>
+                            <th>Varış Saati</th>
+                            <th>Kalkış Günü</th>
+                            <th>Tren Numarası</th>
+                            <th>Ücret</th>
                         </thead>
                         <tbody>
                             <?php
@@ -79,16 +79,16 @@
                                 echo "<td> " . $row['routeId'] . " </td> "
                                     . "<td> " . $row['startingStationTerminal'] . " </td> "
                                     . "<td>" . $row['destinationStationTerminal'] . "</td>"
-                                    . "<td> " . $row['arrivelTime'] . " </td> "
                                     . "<td>" . $row['departureTime'] . "</td>"
+                                    . "<td> " . $row['arrivelTime'] . " </td> "
                                     . "<td>" . $row['dateOfRoute'] . "</td>"
                                     . "<td> " . $row['trainNumber'] . " </td> "
                                     . "<td> " . $row['price'] . " </td> "
                                     . "<form method='post' action='modifyRoute.php?varname=$routeId'>"
-                                    . "<td> <button type='submit' class='btn' name='modifyRoute'>Modify Route</button></a></td>"
+                                    . "<td> <button type='submit' class='btn' name='modifyRoute'>Rotayı Düzenle</button></a></td>"
                                     . "</form>"
                                     . "<form method='post' action='deleteRoute.php?varname=$routeId'>"
-                                    . "<td> <button type='submit' class='btn' name='deleteRoute'>Delete Button</button></a></td>"
+                                    . "<td> <button type='submit' class='btn' name='deleteRoute'>Rotayı Sil</button></a></td>"
                                     . "</form>"
                                     . "</tr>";
                             }
