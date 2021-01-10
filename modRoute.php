f5d42e045a77e778be8a0a41763e7eac20a78c3c
Görkem Deniz SEYHAN <69456932+Denowen@users.noreply.github.com>
2021-01-10 16:54:33 +0300

    Update modRoute.php

--- modRoute.php
+++ modRoute.php
@@ -6,6 +6,7 @@ $routeId = $_POST['routeId'];
 $saat = $_POST['saat'];
 $date = $_POST['date'];
 $price = $_POST['price'];
+$saat2 = $_POST['saat2'];
 
 
 
@@ -13,7 +14,7 @@ if (empty($saat) || empty($date) ||  empty($price)) {
    header("location: modifyRoute.php?varname=$routeId");
 } else {
 
-   $query = "UPDATE route SET departureTime ='$saat:00', dateOfRoute ='$date', price='$price' where routeId='$routeId'";
+   $query = "UPDATE route SET departureTime ='$saat:00', arrivelTime ='$saat2:00', dateOfRoute ='$date', price='$price' where routeId='$routeId'";
    $result = mysqli_query($conn, $query);
    header("location: allRouteList.php");
 }
