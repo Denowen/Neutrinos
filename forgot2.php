051e8d238ae5405af7549f2043e30fbcba8bba35
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 14:47:29 +0300

    Update forgot2.php

--- forgot2.php
+++ forgot2.php
@@ -1,7 +1,25 @@
+<?php
+session_start();
+include('dbconnect.php');
+
+if (isset($_SESSION['loggedin'])) {
+    $email = $_SESSION['email'];
+    //QUERY THAT CHECKS IF THE VIEWER IS AN ADMIN
+    $query = mysqli_query($conn, "select * from registeredusers where regUserEmail = '$email'");
+    $counta = mysqli_num_rows($query);
+    if (!$counta) {
+        header("location:admin.php");
+    } 
+}
+
+
+?>
+
+
+
 <?php 
 
-session_start();
-include 'dbconnect.php';
+
 
 $mail = $_POST['email'];
 
