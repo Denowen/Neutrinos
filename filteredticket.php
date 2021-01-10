3350631e6bfd4565ba57cd937014f439e79d145e
Görkem Deniz SEYHAN <69456932+Denowen@users.noreply.github.com>
2021-01-10 15:20:27 +0300

    Update filteredticket.php

--- filteredticket.php
+++ filteredticket.php
@@ -7,30 +7,10 @@
     <link rel="stylesheet" type="text/css" href="home.css">
     <link rel="stylesheet" type="text/css" href="filteredticket.css">
 </head>
-    
-    <?php
- session_start();
- include 'dbconnect.php';
- $email = $_SESSION['email'];
- $query2 = "SELECT PNR FROM buy where creditCardNumber = '0'";
- $result2 = mysqli_query($conn, $query2);
- $num2 = mysqli_num_rows($result2);
- while ($i < $num2) {
-  while ($count = mysqli_fetch_array($result2)) {
-      $query3 = "DELETE FROM ticket WHERE PNR = '$count[0]'";
-      $result3 = mysqli_query($conn, $query3);
-      $query4 = "DELETE FROM buy WHERE PNR = '$count[0]'";
-      $result4 = mysqli_query($conn, $query4);
-
-   $i++;   
-  }
-}
-
-
-?>
 <body>    
     <?php
-
+    session_start();
+    include 'dbconnect.php';
     include 'delete.php';
     
     if(empty($_POST['way']) && empty($_POST['ticket'])){
