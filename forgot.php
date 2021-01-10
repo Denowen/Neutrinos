b291728a5c98838860a52edf3a812a64e04369c8
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 14:46:55 +0300

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -2,22 +2,22 @@
 <?php include('login.php')?>
 
 <?php
- session_start();
- include 'dbconnect.php';
- $email = $_SESSION['email'];
- $query2 = "SELECT PNR FROM buy where creditCardNumber = '0'";
- $result2 = mysqli_query($conn, $query2);
- $num2 = mysqli_num_rows($result2);
- while ($i < $num2) {
-  while ($count = mysqli_fetch_array($result2)) {
-      $query3 = "DELETE FROM ticket WHERE PNR = '$count[0]'";
-      $result3 = mysqli_query($conn, $query3);
-      $query4 = "DELETE FROM buy WHERE PNR = '$count[0]'";
-      $result4 = mysqli_query($conn, $query4);
+session_start();
+include('dbconnect.php');
 
-   $i++;   
-  }
+if (isset($_SESSION['loggedin'])) {
+    $email = $_SESSION['email'];
+    //QUERY THAT CHECKS IF THE VIEWER IS AN ADMIN
+    $query = mysqli_query($conn, "select * from registeredusers where regUserEmail = '$email'");
+    $counta = mysqli_num_rows($query);
+    if (!$counta) {
+        header("location:admin.php");
+    } 
 }
+
+
+?>
+
 
 
 ?>
