4795b2029af0f9010161e45801ec65ab023efcda
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-01-10 21:32:21 +0300

    Update login.php

--- login.php
+++ login.php
@@ -4,32 +4,33 @@ session_start();
 include('dbconnect.php');
 
 
-
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $query1 = "SELECT * FROM users WHERE userEmail='$email' AND userPassword='$password'";
+    $pasword = md5($password);
+
+    $query1 = "SELECT * FROM registeredusers WHERE regUserEmail='$email' AND regUserPassword='$pasword'";
     $query2 = "SELECT * FROM administrators WHERE adminEmail='$email' AND adminPassword='$password'";
 
     $result1 = mysqli_query($conn, $query1);
     $result2 = mysqli_query($conn, $query2);
 
-    if(mysqli_num_rows($result1) ==1 and mysqli_num_rows($result2) == 0){
-        $_SESSION['email']=$email;
+    if (mysqli_num_rows($result1) == 1) {
+        $_SESSION['loggedin'] = "You logged in";
+        $_SESSION['email'] = $email;
         header('location: home.php');
-    } else if (mysqli_num_rows($result2) == 1){
-        $_SESSION['email']=$email;
+    } else if (mysqli_num_rows($result2) == 1) {
+        $_SESSION['loggedin'] = $email;
+        $_SESSION['success'] = "You logged in";
         header('location: admin.php');
-    }else {echo '<script type="text/javascript">'; 
-echo 'alert("Parola veya Email Yanlış! Lütfen Tekrar Giriniz.");'; 
-echo 'window.location.href = "giris.php";';
-echo '</script>';
-    }
+    } else {
 
+        echo ("<br>wrong id pass");
+    }
 }
 
-if(isset($_POST['register'])){
+if (isset($_POST['register'])) {
     $fname = mysqli_real_escape_string($conn, $_POST['fname']);
     $lname = mysqli_real_escape_string($conn, $_POST['lname']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -39,23 +40,36 @@ if(isset($_POST['register'])){
     $tel = mysqli_real_escape_string($conn, $_POST['tel']);
     $maill = $_POST['email'];
 
-    $query ="INSERT INTO users VALUES('$ssn', '$fname', '$lname', '$email', '$password','0')";
-    $query2 = "INSERT INTO registeredusers VALUES('$fname', '$lname', '$ssn', '$email', '$password', '$bdate', '$tel')";
-    mysqli_query($conn, $query);
-    mysqli_query($conn, $query2);
 
-    $isim = $_POST['fname'];
-    $soyisim = $_POST['lname'];
-    $to_email = "$maill";
-    $subject = ":: Merhaba 👋 Neutrinos Turizm'e hoş geldin ::";
-    $body = "$isim $soyisim Neutrinos Turizm'e kayıt işlemin gerçekleşti. Şu andan itibaren istediğin tren biletini alabilir ve yolculuğa çıkabilirsin.";
-    $headers = "From: neutrinos.turizm@gmail.com";
-    if (mail($to_email, $subject, $body, $headers)) {
-        echo "Email successfully sent to $to_email...";
+    $email_check = "SELECT * FROM registeredusers WHERE regUserEmail='$email'";
+    $result = mysqli_query($conn, $email_check);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) {
+        if ($user['regUserEmail'] === $email) {
+            echo '<script type="text/javascript">';
+            echo 'alert("E-mail zaten kayıtlı.");';
+            echo 'window.location.href = "kayit.php";';
+            echo '</script>';
+        }
     } else {
-        echo "Email sending failed...";
-    }
+        $pasword = md5($password);
 
-    
-    header('location: giris.php');
+        $query = "INSERT INTO users VALUES('$ssn', '$fname', '$lname', '$email', '$pasword')";
+        $query2 = "INSERT INTO registeredusers VALUES('$fname', '$lname', '$ssn', '$email', '$pasword', '$bdate', '$tel')";
+        mysqli_query($conn, $query);
+        mysqli_query($conn, $query2);
+
+        $isim = $_POST['fname'];
+        $soyisim = $_POST['lname'];
+        $to_email = "$maill";
+        $subject = ":: Merhaba 👋 Neutrinos Turizm'e hoş geldin ::";
+        $body = "$isim $soyisim Neutrinos Turizm'e kayıt işlemin gerçekleşti. Şu andan itibaren istediğin tren biletini alabilir ve yolculuğa çıkabilirsin.";
+        $headers = "From: neutrinos.turizm@gmail.com";
+        if (mail($to_email, $subject, $body, $headers)) {
+            echo "Email successfully sent to $to_email...";
+        } else {
+            echo "Email sending failed...";
+        }
+    }
 }
