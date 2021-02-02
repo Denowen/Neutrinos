bb9d3ba0246f86116bc6b1af977782a389cda43e
dogukandoganay <73172947+dogukandoganay@users.noreply.github.com>
2021-02-02 21:53:34 +0300

    Update allTicketList.php

--- allTicketList.php
+++ allTicketList.php
@@ -52,6 +52,9 @@
                     <div class="p-ticket">
                         <a class="a2" href="allTicketList.php">Biletleri Listele</a>
                     </div>
+                    <div class="p-ticket">
+                        <a class="a2" href="addNewTrain.php">Yeni Tren Ekle</a>
+                    </div>
                 </div>
                 <div class="right2">
                     <table cellpadding="20" style="color: rgb(50 239 239 / 68%);flex: 2;">
