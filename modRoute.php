2b82704bc6678f9a04aad996f8b14d1925eda798
Hilal Yavuz <70439086+hilalalyavuz@users.noreply.github.com>
2021-02-02 18:43:47 +0300

    Update modRoute.php

--- modRoute.php
+++ modRoute.php
@@ -29,6 +29,26 @@ if (empty($saat) || empty($date) ||  empty($price)) {
 
    $query = "UPDATE route SET departureTime ='$saat:00', arrivelTime ='$saat2:00', dateOfRoute ='$date', price='$price' where routeId='$routeId'";
    $result = mysqli_query($conn, $query);
+   $query2 = "SELECT r.regUserEmail FROM ticket t, registeredusers r WHERE t.reguserSsn = r.regUserSsn and t.routeId = '$routeId'";
+   $result2 = mysqli_query($conn, $query2);
+   $num2 = mysqli_num_rows($result2);
+   $i = 0;
+   while($i < $num2){
+   while($row = mysqli_fetch_array($result2)){
+   $to_email = $row[$i];
+   $subject = "::📅 Yolculuğunuza bir güncelleme geldi. ::";
+   $body = "Yolculuğunuzun kalkış saati $saat:00, varış saati $saat2:00, ve tarihi $date olarak güncellenmiştir. Detaylara profilinizden ulaşabilirsiniz. :) ";
+   $headers = "From: neutrinos.turizm@gmail.com";
+   if (mail($to_email, $subject, $body, $headers)) {
+        echo "Email successfully sent to $to_email...";
+   } else {
+        echo "Email sending failed...";
+    }
+    $i++;
+
+   }
+   
+   }
    header("location: allRouteList.php");
 }
 else{
